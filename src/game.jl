b929c29101c090c9f679e6d560b4c5c9fd1e0e4f
WestleyArgentum <WestleyArgentum@gmail.com>
2014-09-04 22:52:17 -0400

    starting to load and filter bill data

--- src/game.jl
+++ src/game.jl
@@ -1,13 +1,56 @@
 
 type Game
     teams
-
     bills
-    lobbyists
-
     timeline
 
-    Game() = new(Dict(), Dict(), Dict(), Any[])
+    function Game(bill_data_file)
+        bills = JSON.parse(readall(bill_data_file))
+        timeline = build_timeline(bills)
+
+        new(Dict(), bills, timeline)
+    end
 end
 
 add_lobbyist_to_team(g::Game, teamid, lobbyist) = add_lobbyist(g.teams[teamid], lobbyist)
+
+function filter_overlapping_votes(bills)
+    overlap = Any[]
+    for (k,v) in bills
+        for (k2,v2) in bills
+            if k != k2 && v["num"] == v2["num"] && v["prefix"] == v2["prefix"]
+                if !([k, k2] in overlap) && !([k2, k] in overlap)
+                    push!(overlap, [k, k2])
+                end
+            end
+        end
+    end
+    
+    println(length(overlap), " overlapping actions found...")
+
+    num = 0
+    for (id1, id2) in overlap
+        (haskey(bills, id1) && haskey(bills, id2)) || continue
+
+        passed1 = get(bills[id1], "dateVote", -1)
+        passed2 = get(bills[id2], "dateVote", -2)
+        if passed1 == passed2
+            println("removing: $id1, $id2")
+            num += 1
+            delete!(bills, id1)
+        end
+    end
+
+    println(num, " removed!")
+
+    bills
+end
+
+function filter_has_votes(bills)
+    bills_with_votes = filter((k,b)->get(b, "action", "") == "passage", bills)
+end
+
+function build_timeline(bills)
+    bills_with_votes = filter_has_votes(bills)
+    bill_unique = filter_overlapping_votes(bills_with_votes)
+end
--- src/influence-game.jl
+++ src/influence-game.jl
@@ -2,8 +2,11 @@
 
 module InfluenceGame
 
+using JSON
+
 include("game.jl")
 include("team.jl")
 
+_game = Game("./data/112th-bills.json")
 
 end
