1da12744be2e4b511c5aa210f34f87f3bb689491
WestleyArgentum <WestleyArgentum@gmail.com>
2015-02-24 08:49:00 -0500

    start of stats per industry

--- js/results-ctrl.js
+++ js/results-ctrl.js
@@ -5,7 +5,44 @@
 
         this.restart = function() {
             window.location.reload();
-        }
+        };
+
+        this.calculateIndustryStats = function() {
+            var industryStats = {};
+
+            for (var aid in gameModel.bills) {
+                if (!gameModel.bills.hasOwnProperty(aid)) {
+                    continue;
+                }
+
+                var bill = gameModel.bills[aid];
+                var supporters = bill.positions.support;
+                var opposers = bill.positions.oppose;
+
+                var supportersScore = bill.passed ? 13 : -5;
+                var opposersScore = bill.passed ? -3 : 15;
+
+                for (var i = 0; i < supporters.length; ++i) {
+                    var industry = supporters[i];
+                    industryStats[industry] = industryStats[industry] || 0;
+                    industryStats[industry] += supportersScore;
+                }
+
+                for (var i = 0; i < opposers.length; ++i) {
+                    var industry = opposers[i];
+                    industryStats[industry] = industryStats[industry] || 0;
+                    industryStats[industry] += opposersScore;
+                }
+            }
+
+            gameModel.industryStats = industryStats;
+        };
+
+        this.initialize = function() {
+            this.calculateIndustryStats();
+        };
+
+        this.initialize();
     }]);
 
 })();
